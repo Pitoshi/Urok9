600cca996431d37a90e6f24fdf20b5b95da2a75a
Pitoshi <xalidar@mail.ru>
2024-11-05 01:25:46 +0300

    Выполнены задания урока №9: 1. По описанию класса Player (игрок); 2. По созданию списко дел "DaylyPlanner"; 3. По созанию списка объетов, на примере известных кинофильмов классы "Movie" and "Main"; 4. По созданию хранилища игроков "PlayerStorage"; 5. И по созданию турнирной тадлицы для хранения результатов игры "Tournnament".

--- src/main/java/ru/innopolis/PlayerStorage.java
+++ src/main/java/ru/innopolis/PlayerStorage.java
@@ -21,8 +21,7 @@ import java.util.Set;
         }
 
         public static void main(String[] args) {
-            PlayerStorage Player;
-            Player = new PlayerStorage();
+            PlayerStorage Player = new PlayerStorage();
 
             // Создание 10 игроков
             Player player1 = new Player(1, "PlayerOne", true);
